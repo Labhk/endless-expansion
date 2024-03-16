b1d41c5d137eeb6866c69f74086a907986d4af92
labhk <labh.k2003@gmail.com>
2024-03-16 13:42:59 +0530

    Update word-vista.js

---
+++ pages/word-vista.js
@@ -0,0 +1,13 @@
+
+function WordVista() {
+    // Add your component's logic here
+
+    return (
+        // Add your component's JSX code here
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+export default WordVista;
